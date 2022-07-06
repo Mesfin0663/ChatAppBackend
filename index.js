d35d70b35c564267b7de06ca140fa75ac67495d3
Mesfin Girma <mesfingirmatwo@gmail.com>
2022-07-06 17:01:07 +0300

    removed yarn

--- index.js
+++ index.js
@@ -32,12 +32,8 @@ mongoose.connect(
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
-const corsOptions ={
-   origin:'http://localhost:3000', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 //routes separeted accordingly 
 app.use("/api/users", userRoute); 
