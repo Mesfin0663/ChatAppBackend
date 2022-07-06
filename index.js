3f2392d887db772dc89a53b42f20b7ea59db84fd
Mesfin Girma <mesfingirmatwo@gmail.com>
2022-07-06 17:07:46 +0300

    removed yarn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8800
+const port = process.env.PORT || 5500
 const mongoose = require('mongoose');  // used to handle mongo db
 const dotenv = require("dotenv");
 const helmet = require("helmet");
@@ -45,7 +45,7 @@ app.use("/api/messages", messageRoute);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(8800,()=>{
+app.listen(port,()=>{
     console.log("backend server running!");
 });
 
