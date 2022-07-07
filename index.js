557b8e50e71dc6dc87b3b9574bf2b7fb2a284393
Mesfin Girma <mesfingirmatwo@gmail.com>
2022-07-07 21:36:08 +0300

    first commit

--- index.js
+++ index.js
@@ -44,14 +44,7 @@ app.use("/api/messages", messageRoute);
 //Error handlers
 app.use(notFound);
 app.use(errorHandler);
-app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-   res.header(
-     "Access-Control-Allow-Headers",
-     "Origin, X-Requested-With, Content-Type, Accept"
-   );
-   next();
- });
+
 
 app.listen(port,()=>{
     console.log("backend server running on port!" ,port);
